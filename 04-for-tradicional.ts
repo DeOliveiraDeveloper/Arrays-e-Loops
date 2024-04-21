8cb413c595a5c8ab11b87a7b838d846cd6b01a5b
DeOliveiraDeveloper <monteirocaio4@gmail.com>
2024-04-21 20:08:28 -0300

    Exercicio 3

---
+++ 04-for-tradicional.ts
@@ -0,0 +1,5 @@
+const fruta = ["banana", "Maçã", "Pêra", "Abacaxi"];
+
+for (let i = 0; i < fruta.length; i++) {
+    console.log(fruta[i])
+}
---
+++ exercicio03.ts
@@ -0,0 +1,9 @@
+const numeros = [5, 13, 343, 25, 43];
+
+let resultado = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+    resultado += numeros[i];
+}
+
+console.log(resultado);
